275b6049c31d3bd413d2387304da175a8ad1e490
yozora <2540117053@qq.com>
2024-04-25 18:43:53 +0800

    fix interface bug

--- src/packages/modules/Market/Main/BidderModal.tsx
+++ src/packages/modules/Market/Main/BidderModal.tsx
@@ -1,14 +1,25 @@
-import { Button, useColorModeValue, VStack, Heading, Flex, Text, Box, Image, Input  } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Input,
+  Text,
+  VStack,
+  useColorModeValue,
+} from '@chakra-ui/react'
 
 import BaseModal from '@components/Modal'
 import { ellipseAddress, formatNumberWithCommas } from '@utils'
 import { toastError } from '@utils/toast'
 import { ethers } from 'ethers'
+import FroopyABI from 'packages/abis/demo/fl417.json'
+import { getBidderForm } from 'packages/service/api'
+import { IBidInfo } from 'packages/service/api/types'
+import useStore from 'packages/store'
 import { web3Modal } from 'packages/web3'
 import { useEffect, useMemo, useRef, useState } from 'react'
-import FroopyABI from 'packages/abis/demo/fl417.json'
-import useStore from 'packages/store'
-import { getBidderForm } from 'packages/service/api'
 
 const FL_CONTRACT_ADR = process.env.NEXT_PUBLIC_FL_CONTRACT_ADR
 
@@ -17,23 +28,15 @@ type SubmitOfferModalProps = {
   onClose: () => void
 }
 
-type NFTItem = {
-  userAddress: string;
-  amount: number;
-};
-
-
-
 // 1. 出价列表 - API
 
 // 2. 监听日志，有新的日志拍卖，就前端直接更新列表 - 合约？
 
 // 3. 出价正常 / 异常
 
-
 const BidModal = ({ isOpen, onClose }: SubmitOfferModalProps) => {
   const [value, setValue] = useState('')
-  const [list, setList] = useState<NFTItem[]>([])
+  const [list, setList] = useState<IBidInfo[]>([])
 
   const [availableNums, setAvailableNums] = useState<any>()
 
@@ -42,18 +45,26 @@ const BidModal = ({ isOpen, onClose }: SubmitOfferModalProps) => {
 
   const { address } = useStore()
 
-  const isLowPrice = useMemo(() => list.some(k => Number(value) <= Number(k.amount)), [list, value])
+  const isLowPrice = useMemo(
+    () => list.some((k) => Number(value) <= Number(k.amount)),
+    [list, value],
+  )
 
-  const bidList = useMemo(() => list.slice().sort((a, b) => b.amount - a.amount), [list])
+  const bidList = useMemo(
+    () => list.slice().sort((a, b) => Number(b.amount) - Number(a.amount)),
+    [list],
+  )
 
   const handleBid = async () => {
     if (!value) return toastError('Please bid the price.')
 
+    if (isLowPrice)
+      return toastError('Bid must be higher than the current highest bid.')
 
-    if (isLowPrice) return toastError('Bid must be higher than the current highest bid.')
-
-
-    if (parseFloat(value) > parseFloat(availableNums)) return toastError('Bid must be lower than the current available $OMO Token')
+    if (parseFloat(value) > parseFloat(availableNums))
+      return toastError(
+        'Bid must be lower than the current available $OMO Token',
+      )
 
     const provider = await web3Modal.connect()
     const library = new ethers.providers.Web3Provider(provider)
@@ -64,7 +75,7 @@ const BidModal = ({ isOpen, onClose }: SubmitOfferModalProps) => {
       setBidLoading(true)
 
       const data = await contract.bidLand(ethers.utils.parseEther(value), {
-        gasLimit: BigInt(500000)
+        gasLimit: BigInt(500000),
       })
 
       // const existingItemIndex = bidList.findIndex(item => item.userAddress === address)
@@ -84,7 +95,6 @@ const BidModal = ({ isOpen, onClose }: SubmitOfferModalProps) => {
       console.log(error, '<===')
     }
   }
-
 
   const getAvailableFL = async () => {
     const provider = await web3Modal.connect()
@@ -114,7 +124,12 @@ const BidModal = ({ isOpen, onClose }: SubmitOfferModalProps) => {
     // TODO: 考虑到后续服务端压力，比如 Ddos，需要从合约侧返回的新数据去组装列表
 
     contract.on('NewBids', (Bidder, amount, bidId) => {
-      console.log(Bidder, amount.toString(), bidId.toString(), 'Bidder, amount, bidId')
+      console.log(
+        Bidder,
+        amount.toString(),
+        bidId.toString(),
+        'Bidder, amount, bidId',
+      )
       getBidList().then(() => setBidLoading(false))
     })
   }
@@ -135,76 +150,139 @@ const BidModal = ({ isOpen, onClose }: SubmitOfferModalProps) => {
       variant="bidModal"
       size="2xl"
       isOpen={isOpen}
-      title={<Heading fontSize="22px" lineHeight="32px" textAlign="left">Bid on this Plot of FROMO</Heading>}
+      title={
+        <Heading fontSize="22px" lineHeight="32px" textAlign="left">
+          Bid on this Plot of FROMO
+        </Heading>
+      }
       onClose={onClose}
       bgColor={useColorModeValue ? '#fff' : '#fff'}>
       <VStack align="left">
-        <Text fontSize="14px" lineHeight="20px" color="rgba(0, 0, 0, 0.7)" mb="40px">The highest bidder will have the opportunity to auction their NFT in the next round.</Text>
+        <Text
+          fontSize="14px"
+          lineHeight="20px"
+          color="rgba(0, 0, 0, 0.7)"
+          mb="40px">
+          The highest bidder will have the opportunity to auction their NFT in
+          the next round.
+        </Text>
         <Box>
           <Flex p="10px 20px">
-            <Text w="178px" align="left" mr="82px" fontSize="13px" color="rgba(0, 0, 0, 0.6)">BIDDER</Text>
-            <Text fontSize="13px" color="rgba(0, 0, 0, 0.6)">BID</Text>
+            <Text
+              w="178px"
+              align="left"
+              mr="82px"
+              fontSize="13px"
+              color="rgba(0, 0, 0, 0.6)">
+              BIDDER
+            </Text>
+            <Text fontSize="13px" color="rgba(0, 0, 0, 0.6)">
+              BID
+            </Text>
           </Flex>
-         <Box overflowY="auto" height={bidList.length === 0 ? 0 : '220px'} ref={scrollRef}>
-         {
-          bidList.map((item,v) => (
-            <Flex key={item.userAddress} p="10px 20px" border="1px solid #F2F2F2" borderRadius="10px" align="center" mb="10px">
-              <Flex align="center" w="200px" mr="60px">
-                <Image mr="10px" borderRadius="37px" border="1px solid #F2F2F2" src="/static/account/sidebar/avatar.svg" alt='avatar' w="37px" h="37px"></Image>
-                <Box fontSize="16px" w="160px">
-                  {ellipseAddress(item.userAddress, 6)}
-                </Box>
+          <Box
+            overflowY="auto"
+            height={bidList.length === 0 ? 0 : '220px'}
+            ref={scrollRef}>
+            {bidList.map((item, v) => (
+              <Flex
+                key={item.userAddress}
+                p="10px 20px"
+                border="1px solid #F2F2F2"
+                borderRadius="10px"
+                align="center"
+                mb="10px">
+                <Flex align="center" w="200px" mr="60px">
+                  <Image
+                    mr="10px"
+                    borderRadius="37px"
+                    border="1px solid #F2F2F2"
+                    src="/static/account/sidebar/avatar.svg"
+                    alt="avatar"
+                    w="37px"
+                    h="37px"></Image>
+                  <Box fontSize="16px" w="160px">
+                    {ellipseAddress(item.userAddress, 6)}
+                  </Box>
+                </Flex>
+                <Text
+                  align="left"
+                  flex={1}
+                  fontSize="16px"
+                  color="rgb(0, 0, 0)">
+                  {parseFloat(`${item.amount}`).toFixed(4)} $OMO Token
+                </Text>
+                {item.userAddress === address && (
+                  <Text fontSize="14px" color="#7E4AF1" w="30px">
+                    ME
+                  </Text>
+                )}
               </Flex>
-              <Text align="left" flex={1} fontSize="16px" color="rgb(0, 0, 0)">{parseFloat(`${item.amount}`).toFixed(4)} $OMO Token</Text>
-              {
-                item.userAddress === address && (<Text fontSize="14px" color="#7E4AF1" w="30px">ME</Text>)
-              }
-            </Flex>
-          ))
-         }
-         </Box>
+            ))}
+          </Box>
         </Box>
         <Flex align="baseline">
-            <Box mr="14px">
-              <Flex w="264px" p="16px" borderRadius="10px" alignItems="center" bg="#F4F4F4">
-                <Text>Bid:</Text>
-                <Input
-                  _focusVisible={{
-                    borderWidth: '0px',
-                  }}
-                  type="number"
-                  fontWeight={700}
-                  fontSize="20px"
-                  border="none"
-                  onChange={e => setValue(e.target.value)}
-                />
-                <Text color="#333" fontSize="14px" lineHeight="24px">$OMO</Text>
-              </Flex>
-            </Box>
-            <Button
-              w="298px"
+          <Box mr="14px">
+            <Flex
+              w="264px"
+              p="16px"
               borderRadius="10px"
-              fontSize="20px"
-              fontWeight="700"
-              h="66px"
-              color="#fff"
-              bg="#704BEA"
-              _hover={{ bg: "#704BEA" }}
-              onClick={handleBid}
-              disabled={availableNums <= 0 || bidLoading}
-              isLoading={bidLoading}
-            >
-              Bid
-            </Button>
+              alignItems="center"
+              bg="#F4F4F4">
+              <Text>Bid:</Text>
+              <Input
+                _focusVisible={{
+                  borderWidth: '0px',
+                }}
+                type="number"
+                fontWeight={700}
+                fontSize="20px"
+                border="none"
+                onChange={(e) => setValue(e.target.value)}
+              />
+              <Text color="#333" fontSize="14px" lineHeight="24px">
+                $OMO
+              </Text>
+            </Flex>
+          </Box>
+          <Button
+            w="298px"
+            borderRadius="10px"
+            fontSize="20px"
+            fontWeight="700"
+            h="66px"
+            color="#fff"
+            bg="#704BEA"
+            _hover={{ bg: '#704BEA' }}
+            onClick={handleBid}
+            disabled={availableNums <= 0 || bidLoading}
+            isLoading={bidLoading}>
+            Bid
+          </Button>
         </Flex>
-        <Text mt="8px" fontSize="12px" lineHeight="18px" color="#4F4F4F">Available：{formatNumberWithCommas(availableNums)} $OMO Token</Text>
-        <Flex bg="#F6BF324D" p='15px' borderRadius="8px" mt="24px">
-            <Image src="/static/common/info.svg" alt="info" w='16px' h="16px" mr="10px" />
-            <Text textAlign="justify" color="#000" fontSize="14px" lineHeight="21px" mt="-5px">
-              The $OMO you bid is used to purchase the FROMO plot. It will be locked until the bidding ends.
-              If you lose the FROMO plot, it will be unlocked after the bidding ends. The FROMO plot winner who failed to stake NFT will lose the $OMO he/she bid.
-            </Text>
-         </Flex>
+        <Text mt="8px" fontSize="12px" lineHeight="18px" color="#4F4F4F">
+          Available：{formatNumberWithCommas(availableNums)} $OMO Token
+        </Text>
+        <Flex bg="#F6BF324D" p="15px" borderRadius="8px" mt="24px">
+          <Image
+            src="/static/common/info.svg"
+            alt="info"
+            w="16px"
+            h="16px"
+            mr="10px"
+          />
+          <Text
+            textAlign="justify"
+            color="#000"
+            fontSize="14px"
+            lineHeight="21px"
+            mt="-5px">
+            The $OMO you bid is used to purchase the FROMO plot. It will be
+            locked until the bidding ends. If you lose the FROMO plot, it will
+            be unlocked after the bidding ends. The FROMO plot winner who failed
+            to stake NFT will lose the $OMO he/she bid.
+          </Text>
+        </Flex>
       </VStack>
     </BaseModal>
   )
