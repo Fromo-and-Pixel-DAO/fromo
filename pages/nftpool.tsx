def7e78c8dacaa4d9d65afbeded6ed81b8a590d4
nvphung22 <nvphung22@gmail.com>
2024-06-14 17:50:32 +0700

    fix style nftpool page

--- pages/nftpool.tsx
+++ pages/nftpool.tsx
@@ -34,9 +34,9 @@ const NFTpool = () => {
         <>
           <Box
             mt={{ base: '28px', md: '36px', xl: '52px' }}
-            px="40px"
+            px={{ base: '16px', md: '24px', lg: '32px', xl: '48px' }}
             minH="60vh">
-            <SimpleGrid columns={[1, 2, 2, 3, 5, 6]} spacing="20px">
+            <SimpleGrid columns={[1, 2, 2, 3, 5, 5]} spacing="20px">
               {gameNft.nftList.map((item, idx) => {
                 return (
                   <ItemGrid gridName="finishedList" item={item} key={idx} />
