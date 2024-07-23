1c7a6cb4a970da6ccbf4588ca5ec945bb67c752f
nvphung22 <nvphung22@gmail.com>
2024-07-23 16:24:13 +0700

    update style toast

--- src/packages/utils/toast/index.ts
+++ src/packages/utils/toast/index.ts
@@ -1,23 +1,43 @@
+import IconError from '@components/Icon/icon-error'
+import IconSuccess from '@components/Icon/icon-success'
+import IconWarning from '@components/Icon/icon-warning'
+import React from 'react'
 import { Flip, toast } from 'react-toastify'
 
 export const toastSuccess = (message: string, delay = 3000): void => {
   if (message) {
     if (delay) {
       toast.success(message, {
+        icon: React.createElement(IconSuccess),
         position: toast.POSITION.TOP_CENTER,
         pauseOnHover: true,
         transition: Flip,
         autoClose: delay,
+        closeButton: false,
         style: {
-          color: 'black',
+          color: 'white',
+          background: '#00B25D',
+          fontFamily: 'Montserrat',
+          fontWeight: '500',
+          borderRadius: '40px',
+          padding: '16px',
+          minHeight: '56px',
         },
       })
     } else {
       toast.success(message, {
+        icon: React.createElement(IconSuccess),
         position: toast.POSITION.TOP_CENTER,
+        closeButton: false,
         autoClose: false,
         style: {
-          color: 'black',
+          color: 'white',
+          background: '#00B25D',
+          fontFamily: 'Montserrat',
+          fontWeight: '500',
+          borderRadius: '40px',
+          padding: '16px',
+          minHeight: '56px',
         },
       })
     }
@@ -28,21 +48,37 @@ export const toastWarning = (message: string, delay = 3000): void => {
   if (message) {
     if (delay) {
       toast.warning(message, {
+        icon: React.createElement(IconWarning),
         position: toast.POSITION.TOP_CENTER,
+        closeButton: false,
         pauseOnHover: true,
         transition: Flip,
         autoClose: delay,
         style: {
           color: 'black',
+          background: '#FFBD13',
+          fontFamily: 'Montserrat',
+          fontWeight: '500',
+          borderRadius: '40px',
+          padding: '16px',
+          minHeight: '56px',
         },
       })
     } else {
       toast.warning(message, {
+        icon: React.createElement(IconWarning),
         position: toast.POSITION.TOP_CENTER,
+        closeButton: false,
         transition: Flip,
         autoClose: false,
         style: {
           color: 'black',
+          background: '#FFBD13',
+          fontFamily: 'Montserrat',
+          fontWeight: '500',
+          borderRadius: '40px',
+          padding: '16px',
+          minHeight: '56px',
         },
       })
     }
@@ -61,22 +97,38 @@ export const toastError = (error: ErrorMsg, delay = 3000) => {
   if (toastData && typeof toastData === 'string' && toastData !== '') {
     if (delay) {
       toast.error(toastData, {
+        icon: React.createElement(IconError),
         position: toast.POSITION.TOP_CENTER,
+        closeButton: false,
         pauseOnHover: true,
         transition: Flip,
         autoClose: delay,
         style: {
-          color: 'black',
+          color: 'white',
+          background: '#F95050',
+          fontFamily: 'Montserrat',
+          fontWeight: '500',
+          borderRadius: '40px',
+          padding: '16px',
+          minHeight: '56px',
         },
       })
     } else {
       toast.error(toastData, {
+        icon: React.createElement(IconError),
         position: toast.POSITION.TOP_CENTER,
+        closeButton: false,
         pauseOnHover: true,
         transition: Flip,
         autoClose: false,
         style: {
-          color: 'black',
+          color: 'white',
+          background: '#F95050',
+          fontFamily: 'Montserrat',
+          fontWeight: '500',
+          borderRadius: '40px',
+          padding: '16px',
+          minHeight: '56px',
         },
       })
     }
