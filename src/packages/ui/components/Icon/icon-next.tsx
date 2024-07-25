b216feb2b92a98f0da9b8a54e86570cca8973af5
nvphung22 <nvphung22@gmail.com>
2024-07-25 11:51:18 +0700

    add navigation buttons

---
+++ src/packages/ui/components/Icon/icon-next.tsx
@@ -0,0 +1,15 @@
+import { Icon } from '@chakra-ui/react'
+
+export default function IconNext() {
+  return (
+    <Icon width="40px" height="40px" viewBox="0 0 40 40" fill="none">
+      <circle cx="20" cy="20" r="20" fill="#1DFED6" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M18 8.68627L15.8787 10.8076L25.0711 20L15.8786 29.1924L18 31.3137L27.1924 22.1213L27.1924 22.1213L29.3137 20L18 8.68627Z"
+        fill="#2F2B50"
+      />
+    </Icon>
+  )
+}
--- src/packages/ui/components/NFTAuctions/index.tsx
+++ src/packages/ui/components/NFTAuctions/index.tsx
@@ -1,18 +1,21 @@
 'use client'
 
 import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react'
-import ItemGrid from 'packages/ui/components/ListItems/ItemGrid'
-
 import NoData from '@components/NoData'
 import useFomoStore from 'packages/store/fomo'
+import ItemGrid from 'packages/ui/components/ListItems/ItemGrid'
 
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 
 import { Spinner } from '@chakra-ui/react'
+import IconNext from '@components/Icon/icon-next'
+import { useWindowSize } from '@hooks/useWindowSize'
 
 export default function NFTAuctions() {
+  const { width } = useWindowSize()
   const { gameList, upcomingList, ongoingList, finishedList, getNftAuctions } =
     useFomoStore()
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   return (
     <>
@@ -46,31 +49,87 @@ export default function NFTAuctions() {
                   <Text>Ongoing NFT Auctions</Text>({ongoingList.length})
                 </Flex>
               </Box>
-              <Box pos="relative">
-                {ongoingList.length > 0 && (
-                  <Flex
-                    gap="20px"
-                    overflow="auto"
-                    pb="8px"
-                    sx={{
-                      '::-webkit-scrollbar': {
-                        display: 'none',
-                      }, // Chrome, Safari and Opera
-                      'scrollbar-width': 'none', // Firefox
-                      '-ms-overflow-style': 'none', // IE and Edge
-                    }}>
-                    {ongoingList?.map((item, idx) => {
-                      return (
-                        <ItemGrid
-                          isOngoingList
-                          gridName="ongoingList"
-                          item={item}
-                          key={idx}
-                        />
-                      )
-                    })}
-                  </Flex>
-                )}
+              <Box h={{ base: '430px', md: '226px' }} overflow="hidden">
+                <Box pos="relative" overflowX="auto">
+                  {ongoingList.length > 0 && (
+                    <Flex
+                      w="fit-content"
+                      gap="20px"
+                      pb="8px"
+                      sx={{
+                        '::-webkit-scrollbar': {
+                          display: 'none',
+                        },
+                        'scrollbar-width': 'none',
+                        '-ms-overflow-style': 'none',
+                      }}
+                      style={{
+                        transform: `translateX(-${
+                          width > 768 ? currentIndex * 50 : currentIndex * 100
+                        }%)`,
+                        transition: 'transform 0.5s ease-in-out',
+                      }}>
+                      {ongoingList.map((item, idx) => (
+                        <Box key={idx} minW="fit-content">
+                          <ItemGrid
+                            isOngoingList
+                            gridName="ongoingList"
+                            item={item}
+                          />
+                        </Box>
+                      ))}
+                    </Flex>
+                  )}
+
+                  {currentIndex > 0 && (
+                    <Box
+                      width="40px"
+                      height="40px"
+                      left={{ base: '5%', md: '5%', xl: '3%' }}
+                      zIndex={10}
+                      bottom={{
+                        base: currentIndex > 0 ? '53.8%' : '50%',
+                        md: currentIndex > 0 ? '56.5%' : '50%',
+                      }}
+                      cursor="pointer"
+                      _hover={{ opacity: '0.8' }}
+                      transform="translateY(-50%) rotate(180deg)"
+                      pos="sticky"
+                      onClick={() => setCurrentIndex(currentIndex - 1)}>
+                      <IconNext />
+                    </Box>
+                  )}
+
+                  {ongoingList.length > 0 && (
+                    <Box
+                      width="40px"
+                      display={{
+                        base: ongoingList.length >= 2 ? 'block' : 'none',
+                        md: ongoingList.length >= 2 ? 'block' : 'none',
+                        lg:
+                          ongoingList.length >= 2 && width >= 1024
+                            ? ongoingList.length >= 3
+                              ? 'block'
+                              : 'none'
+                            : 'block',
+                        xl: ongoingList.length >= 3 ? 'block' : 'none',
+                      }}
+                      height="40px"
+                      left={{ base: '83%', md: '90%', xl: '94%' }}
+                      zIndex={10}
+                      bottom={{
+                        base: currentIndex > 0 ? '53.8%' : '50%',
+                        md: currentIndex > 0 ? '56.5%' : '50%',
+                      }}
+                      cursor="pointer"
+                      _hover={{ opacity: '0.8' }}
+                      transform="translateY(-50%)"
+                      pos="sticky"
+                      onClick={() => setCurrentIndex(currentIndex + 1)}>
+                      <IconNext />
+                    </Box>
+                  )}
+                </Box>
               </Box>
             </Box>
 
